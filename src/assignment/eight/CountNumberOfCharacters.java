44b26afb51773b59be5cb2161b42e21384f1f275
Andrew Werling <andrewwerling@Andrews-MacBook-Pro.local>
2021-02-25 17:35:01 -0500

    Add existing file

--- src/assignment/eight/CountNumberOfCharacters.java
+++ src/assignment/eight/CountNumberOfCharacters.java
@@ -3,6 +3,11 @@
  */
 package assignment.eight;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  * @author andrewwerling
  *
@@ -13,8 +18,49 @@ public class CountNumberOfCharacters {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		// Scanner object to get user input
+		Scanner userInput = new Scanner(System.in);
 		
+		char chosenCharacter = 'e';
 		
+		System.out.println(countCharacter(chosenCharacter));
+		
+		userInput.close();
+	}
+
+	static Integer countCharacter(char chosenCharacter) {
+		// Integer to count occurrences of chosenCharacter
+		Integer characterCount = 0;
+
+		try {
+			
+			// file to search in
+			String directoryPath = "appendToThisFile.txt";
+
+			// Create a new file object from directoryPath
+			File chosenFile = new File(directoryPath);
+			
+			
+			BufferedReader inputStream = new BufferedReader(new FileReader(chosenFile));
+			
+			String line = null;
+			while ((line = inputStream.readLine()) !=null) {
+				for (int i = 0; i < line.length() ; i++) {
+					if(line.charAt(i) == chosenCharacter) {
+						characterCount++;
+					}
+				}
+			}
+
+			inputStream.close();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return characterCount;
 
 	}
 
