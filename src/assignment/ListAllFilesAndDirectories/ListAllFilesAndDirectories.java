fbcc5005896ff132561b3fd2b76001976190ca0c
andrewwerling <andrewwerling@andrews-mbp.hsd1.fl.comcast.net>
2021-02-27 21:32:26 -0500

    Updated File Directory to be entered from console/command line by user via scanner

--- src/assignment/ListAllFilesAndDirectories/ListAllFilesAndDirectories.java
+++ src/assignment/ListAllFilesAndDirectories/ListAllFilesAndDirectories.java
@@ -4,6 +4,8 @@
 package assignment.ListAllFilesAndDirectories;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.util.Scanner;
 /**
  * @author andrewwerling
  *
@@ -16,19 +18,26 @@ public class ListAllFilesAndDirectories {
 	public static void main(String[] args) {
 		
 		try {
-			// put given directory here as String value
-			String directoryPath = "/Users/andrewwerling/Documents";
-			
-			// Create a new file object from directoryPath
-			File directoryAsFile = new File(directoryPath);
-			
-			// Create string array of all files/directories from directoryAsFile object
-			String[] fileList = directoryAsFile.list();
-			
-			// Loop through fileList and print name of file/directory
-			for (String fileName: fileList) {
-				System.out.println(fileName);
+			Path filePath = null;
+			Scanner userInput = new Scanner(System.in);
+
+			while (filePath == null) {
+				
+				System.out.println("Please enter the desired directory to search (absolute or relative file path:");
+				// Get desired file path from user input via scanner 
+				String directoryPath = userInput.next();
+				
+				// Create a new file object from directoryPath
+				File directoryAsFile = new File(directoryPath);
+				// Create string array of all files/directories from directoryAsFile object
+				String[] fileList = directoryAsFile.list();
+				// Loop through fileList and print name of file/directory
+				for (String fileName : fileList) {
+					System.out.println(fileName);
+				}
 			}
+			userInput.close();
+			System.exit(0);
 			
 		} catch (Exception FileNotFoundException) {
 			
