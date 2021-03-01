9d0c3b186a8622497fe3befc70c02e10ead4cd19
andrewwerling <andrewwerling@andrews-mbp.lan>
2021-03-01 14:20:25 -0500

    Basic Lambdas (Sort shortest to longest and vice versa)

---
+++ src/assignment/BasicLambdas/BasicLambdas.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package assignment.BasicLambdas;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+
+/**
+ * @author andrewwerling
+ *
+ */
+public class BasicLambdas {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Array to sort
+		List<String> song = Arrays.asList("gently", "down", "the", "stream");
+		
+		System.out.println("Sort Array from shortest to longest\n-------------------------------");
+		// Sort array from shortest to longest
+		Comparator<String> compLengthShortest = (aName, bName) -> aName.length() - bName.length();
+		song.stream().sorted(compLengthShortest).forEach(System.out::println);
+		
+		System.out.println("\nSort Array from longest to shortest\n-------------------------------");
+		// Sort array from longest to shortest
+		Comparator<String> compLengthLongest = (aName, bName) -> bName.length() - aName.length();
+		song.stream().sorted(compLengthLongest).forEach(System.out::println);
+
+	}
+
+}
